30655f28312aa9921b6976a1af13f7cd052318c7
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-03-21 10:10:35 -0400

    add nav and about and home page, fixed node modules issue

--- client/MoodMusic/src/App.jsx
+++ client/MoodMusic/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import HomePage from './components/pages/HomePage/HomePage';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <main>
-
-      </main>
-
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
