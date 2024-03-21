55012c762155c268395a120518e9e7a40c84bc28
Cob007 <godwill2beracah@outlook.com>
2024-03-21 16:01:25 -0400

    audio added

--- client/MoodMusic/src/components/SongCards/SongCard.jsx
+++ client/MoodMusic/src/components/SongCards/SongCard.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import "./SongCard.scss"
-import playbutton from "../../assets/images/play.png"
-import dummyImg from "../../assets/images/dummyImg.jpeg"
+import React, { useEffect, useState } from "react";
+import "./SongCard.scss";
+import playbutton from "../../assets/images/play.png";
+import dummyImg from "../../assets/images/dummyImg.jpeg";
 
-const SongCard = () => {
-    return (
-        <section className='songCard'>
-            <div className='songImgContainer'>
-                <img className='songImg' src={dummyImg} alt="song img" />
-            </div>
+const SongCard = (props) => {
+  const { audio } = props;
+  console.log(audio);
 
-            <div className='songInfo'>
-                <h3>Song: </h3>
-                <h3>Artist: </h3>
-                <p>Description: </p>
-            </div>
+  const BASE_URL = "http://localhost:8080";
+  const imgUrl = `${BASE_URL}${audio?.poster}`;
+  const _audioUrl = `${BASE_URL}${audio?.url}`;
+
+  const [audioUrl] = useState(new Audio(_audioUrl));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audioUrl.play() : audioUrl.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audioUrl.addEventListener('ended', () => setPlaying(false));
+    return () => {
+        audioUrl.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
 
 
+  return (
+    <section className="songCard">
+      <div className="songImgContainer">
+        <img className="songImg" src={imgUrl} alt="song img" />
+      </div>
 
-        </section>
-    )
-}
+      <div className="songInfo">
+        <h3>Song: {audio?.title}</h3>
+        <h3>Artist: {audio?.author}</h3>
+        <p>Description: {audio?.desciption} </p>
+        <p onClick={toggle} className="playButton">
+            {playing ? "Pause" : "Play"}
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default SongCard
+export default SongCard;
--- client/MoodMusic/src/components/pages/SongsPage/SongsPage.jsx
+++ client/MoodMusic/src/components/pages/SongsPage/SongsPage.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import "./SongsPage.scss"
-import Nav from "../../../components/Nav/Nav"
-import SongCard from '../../SongCards/SongCard'
-
+import React, { useEffect, useState } from "react";
+import "./SongsPage.scss";
+import Nav from "../../../components/Nav/Nav";
+import SongCard from "../../SongCards/SongCard";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const SongsPage = () => {
-    return (
-        <section>
-            <Nav />
+  const { userId } = useParams();
+  const [audioList, setAudioList] = useState([]);
 
-            <div className='cardDisplay'>
-                <SongCard />
-                <SongCard />
-                <SongCard />
-            </div>
-        </section>
-    )
-}
+  const BASE_URL = "http://localhost:8080";
 
-export default SongsPage
+  const getMusicbyMood = async (mood) => {
+    try {
+      const res = await axios.get(`${BASE_URL}/api/audio/${mood}`);
+      console.log(res.data.data);
+      setAudioList((prev) => res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getUserMood = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/api/profile/${userId}`);
+      const userMood = res.data.data.mood;
+      console.log(userMood);
+      getMusicbyMood(userMood);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserMood();
+  }, []);
+
+  return (
+    <section>
+      <Nav />
+
+      <div className="cardDisplay">
+        {audioList.map((audio, index) => (
+          <div key={index}>
+            <SongCard audio={audio}/>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SongsPage;
