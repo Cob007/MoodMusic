757f4804a754a76b39c1a86f888ee65172f21852
Cob007 <godwill2beracah@outlook.com>
2024-03-21 12:47:28 -0400

    Public audio part activated

--- server/server.js
+++ server/server.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
+const port = process.env.PORT || 8080;
 
-const port = process.env.PORT || 8080
+const profileRouter = require("./routes/profile");
+const audioRouter = require("./routes/audio");
 
-const profileRouter = require('./routes/profile')
-const audioRouter = require('./routes/audio')
+app.use("/testing", (req, res) => {
+  res.json("All our apis are working as expected! thanks ^=^");
+});
 
-app.use("/testing", (req, res)=>{
-    res.json("All our apis are working as expected! thanks ^=^")
-})
+app.use("/api", [profileRouter, audioRouter]);
 
-app.use('/api', [profileRouter, audioRouter])
-
-app.listen(port, ()=> {
-    console.log(`Server is running late at ${port}`)
-})
-
-
-
+app.listen(port, () => {
+  console.log(`Server is running late at ${port}`);
+});
