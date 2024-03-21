75df92d35fe655b9e3e2ad26802dd2070c19c7a9
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-03-21 14:25:07 -0400

    drop down and second page starter

--- client/MoodMusic/src/App.jsx
+++ client/MoodMusic/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import HomePage from './components/pages/HomePage/HomePage';
+import SongsPage from './components/pages/SongsPage/SongsPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/songs" element={<SongsPage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- client/MoodMusic/src/components/pages/SongsPage/SongsPage.jsx
+++ client/MoodMusic/src/components/pages/SongsPage/SongsPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import "./SongsPage.scss"
 import Nav from "../../../components/Nav/Nav"
+import SongCard from '../../SongCards/SongCard'
 
 
 const SongsPage = () => {
     return (
         <section>
             <Nav />
+
+            <div className='cardDisplay'>
+                <SongCard />
+                <SongCard />
+                <SongCard />
+            </div>
         </section>
     )
 }
