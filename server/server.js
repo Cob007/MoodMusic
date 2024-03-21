fbf89268e453ebe1dcd6f19d5fa79eb10acde261
Cob007 <godwill2beracah@outlook.com>
2024-03-21 12:36:26 -0400

    Endpoint for profile and audio added

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ app.use(express.json());
 
 const port = process.env.PORT || 8080
 
-const createProfileRouter = require('./routes/profile')
+const profileRouter = require('./routes/profile')
+const audioRouter = require('./routes/audio')
 
 app.use("/testing", (req, res)=>{
     res.json("All our apis are working as expected! thanks ^=^")
 })
 
-app.use('/api', createProfileRouter)
+app.use('/api', [profileRouter, audioRouter])
 
 app.listen(port, ()=> {
     console.log(`Server is running late at ${port}`)
