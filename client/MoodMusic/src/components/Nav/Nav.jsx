adeffe696a56b834aa3fd932dd5056ab5b997f8c
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-03-21 14:54:07 -0400

    nav bar and second page

--- client/MoodMusic/src/components/Nav/Nav.jsx
+++ client/MoodMusic/src/components/Nav/Nav.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.scss';
+import logo from "../../assets/images/logo.png"
 
 const Nav = () => {
     return (
         <div className='nav'>
+            <img className='logoSongPage' src={logo} alt="mood music logo" />
+            {/* 
             <ul className='listParent'>
                 <li className='listItem'><Link to="/">Homepage</Link></li>
                 <li className='listItem'><Link to="/about">About</Link></li>
-            </ul>
+            </ul> */}
+
+
         </div>
     );
 };
